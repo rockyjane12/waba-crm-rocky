e7cf70037c37888fbf72afb06a2c3dbd02d51b1c
rockyjane12 <rockyjane1232@gmail.com>
2025-06-23 19:50:33 +0530

    Fix Next.js image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,6 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === "production",
   },
   images: {
-    protocol: 'https',
     formats: ["image/avif", "image/webp"],
     domains: ["images.pexels.com"],
   },
